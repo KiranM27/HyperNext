0c500c83e027535e0c972c2810cdbdf75dfa638c
Kiran Mohan <58102994+KiranM27@users.noreply.github.com>
2023-07-01 16:52:38 +0800

    feat: store theme prefs in local storage

--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -6,27 +6,36 @@ import DarkModeIcon from '@/public/icons/DarkModeIcon';
 const ThemeChanger = () => {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+  const THEME_STORAGE_KEY = '@theme';
+  const LIGHT_THEME = 'light';
+  const DARK_THEME = 'dark';
 
   // When mounted on client, now we can show the UI
   useEffect(() => {
     // check if the local storage has a theme set
-    const localTheme = window?.localStorage?.getItem('@theme') || 'light';
+    const localTheme = window?.localStorage?.getItem(THEME_STORAGE_KEY) ?? LIGHT_THEME;
     setTheme(localTheme);
     setMounted(true);
   }, []);
+
+  const handleThemeChange = () => {
+    const newTheme = theme === LIGHT_THEME ? DARK_THEME : LIGHT_THEME;
+    setTheme(newTheme);
+    window?.localStorage?.setItem(THEME_STORAGE_KEY, newTheme);
+  };
 
   if (!mounted) return null;
 
   return (
     <div className="flex items-center">
-      {theme === 'dark' ? (
-        <button onClick={() => setTheme('light')} className="text-white rounded-full outline-none focus:outline-none">
+      {theme === DARK_THEME ? (
+        <button onClick={handleThemeChange} className="text-white rounded-full outline-none focus:outline-none">
           <span className="sr-only">Light Mode</span>
           <LightModeIcon />
         </button>
       ) : (
         <button
-          onClick={() => setTheme('dark')}
+          onClick={handleThemeChange}
           className="text-black rounded-full outline-none focus:outline-none focus-visible:ring focus-visible:ring-gray-100 focus:ring-opacity-20"
         >
           <span className="sr-only">Dark Mode</span>
