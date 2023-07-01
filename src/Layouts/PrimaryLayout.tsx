e02e02c911aaa20a2ea1f67d1acf2b88d8de3db8
Kiran Mohan <58102994+KiranM27@users.noreply.github.com>
2023-07-01 15:18:54 +0800

    feat: update meta data via the primary layout

--- src/Layouts/PrimaryLayout.tsx
+++ src/Layouts/PrimaryLayout.tsx
@@ -1,14 +1,24 @@
 import Container from '@/components/Container';
 import Navbar from '@/components/Navbar';
+import Head from 'next/head';
 import React from 'react';
 
 type Props = {
   children: React.ReactNode;
+  title?: string;
+  description?: string;
 };
 
-const PrimaryLayout: React.FC<Props> = ({ children }) => {
+const PrimaryLayout: React.FC<Props> = ({ children, title, description }) => {
   return (
     <>
+      {/* page meta data */}
+      <Head>
+        <title>{title || 'HyperNext'}</title>
+        <meta name="description" content={description || 'Ignite your Next.js projects with enhanced productivity and unleash boundless development possibilities.'} />
+      </Head>
+      {/* end of page meta data */}
+
       <Navbar />
       <Container>{children}</Container>
     </>
