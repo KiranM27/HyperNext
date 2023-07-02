3e59381f97173de30a6a5cfbad11d390d3088614
Kiran Mohan <58102994+KiranM27@users.noreply.github.com>
2023-07-02 18:10:12 +0800

    feat: wrapped app with redux provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
+import { store } from '@/redux/store';
 import '@/styles/globals.css';
 import { AppPropsWithLayout } from '@/types/page';
 import { ThemeProvider } from 'next-themes';
+import { Provider as ReduxProvider } from 'react-redux';
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return <ThemeProvider attribute="class">{getLayout(<Component {...pageProps} />)}</ThemeProvider>;
+  return (
+    <ReduxProvider store={store}>
+      <ThemeProvider attribute="class">{getLayout(<Component {...pageProps} />)}</ThemeProvider>
+    </ReduxProvider>
+  );
 }
